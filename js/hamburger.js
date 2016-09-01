6a4b4498debf52428856d54685b62fc0f1a76a29
Damian Fiałkiewicz <aekal4@gmail.com>
2016-09-01 23:22:14 +0200

    Menu class rename & tablet scale

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,10 @@
 $(".hamburger").click(function() {
-  $(".menu__row").toggleClass("menu__row--visible");
+  $(".menu__list").toggleClass("menu__list--visible");
   $(".form-search").removeClass("form-search--visible");
 });
 
 $(".btn-search-mobile").click(function() {
   $(".form-search").toggleClass("form-search--visible");
-  $(".menu__row").removeClass("menu__row--visible");
+  $(".menu__list").removeClass("menu__list--visible");
   document.querySelector(".form-search__field").focus();
 });
