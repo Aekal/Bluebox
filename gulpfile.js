1d4bdaec34a1ad3310481c26f05ae7a4e9b216ca
Damian Fiałkiewicz <aekal4@gmail.com>
2016-09-20 02:05:07 +0200

    CSS & JS minify with maps

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var cssbeautify = require('gulp-cssbeautify');
-var uglify = require('gulp-uglifyjs');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -16,34 +18,38 @@ gulp.task('browserSync', function() {
 
 gulp.task('watch', ['browserSync'], function() {
   gulp.watch('*.html', browserSync.reload);
-  gulp.watch('style.css', browserSync.reload);
+  gulp.watch('css/style.css', browserSync.reload);
   gulp.watch('js/*.js', browserSync.reload);
 });
 
 gulp.task('autoprefixer', function() {
-  gulp.src('style.css')
-    .pipe(autoprefixer())
+  gulp.src('css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['Safari >= 6.1', 'IE >= 10', 'Firefox >= 28'],
+              cascade: false}))
     .pipe(gulp.dest('prefixed-css'))
 });
 
 gulp.task('beautify', function(){
-  gulp.src('style.css')
+  gulp.src('css/style.css')
     .pipe(cssbeautify())
     .pipe(gulp.dest('beauty-css'))
-});
-
-gulp.task('uglify', function() {
-  gulp.src('js/*.js')
-    .pipe(uglify('main.min.js', {
-      outSourceMap: true
-    }))
-    .pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('minify-css', function() {
     return gulp.src('css/style.css')
         .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('css-map'))
-        .pipe(gulp.dest('dist/css'))
+          .pipe(concat('style.min.css'))
+          .pipe(cleanCSS())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('maps'))
+});
+
+gulp.task('uglify', function() {
+  gulp.src('js/main.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('maps'));
 });
